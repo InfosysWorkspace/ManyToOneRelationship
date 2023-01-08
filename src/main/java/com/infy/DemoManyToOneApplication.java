efa7a17522ee82c9b5dcd2ad1c29f2238e540af3
BsoftMaewada <maewada@live.com>
2023-01-08 03:44:42 -0500

    initial commit

---
+++ src/main/java/com/infy/DemoManyToOneApplication.java
@@ -0,0 +1,13 @@
+package com.infy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoManyToOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoManyToOneApplication.class, args);
+	}
+
+}
