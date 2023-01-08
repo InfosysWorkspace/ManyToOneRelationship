8e11cfe9e4e5f4222ace82b9b2ff5760658915ad
BsoftMaewada <maewada@live.com>
2023-01-08 05:34:01 -0500

    added repository

---
+++ src/main/java/com/infy/repository/LoanRepository.java
@@ -0,0 +1,8 @@
+package com.infy.repository;
+
+import com.infy.entity.Loan;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LoanRepository extends CrudRepository<Loan, Integer> {
+
+}
